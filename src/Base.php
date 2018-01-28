590bdfbc5de830d9afb0ce5599d27b8ada628c6d
ahmjw <rawndummy@gmail.com>
2018-01-28 15:25:26 +0900

    Add DB connection for model, handlers, page menu on admin.

--- src/Base.php
+++ src/Base.php
@@ -5,25 +5,78 @@ namespace Introvesia\WpChupooMvc;
 use Introvesia\PhpDomView\Layout;
 use Introvesia\PhpDomView\View;
 use Introvesia\PhpDomView\Config as ViewConfig;
+use Introvesia\Chupoo\Models\Db;
 
 class Base
 {
 	public function run()
 	{
+		global $wpdb;
+
 		ViewConfig::setData(array(
 			'base_url' => get_home_url(),
 			'layout_dir' => get_template_directory() . '/modules/layouts',
 			'layout_url' => get_template_directory_uri(),
 		));
+
+		Db::setConnection($wpdb->dbh);
+
+		spl_autoload_register(array($this, 'loadClass'));
+		set_exception_handler(array($this, 'handleException'));
 		add_action( 'template_redirect', array($this, 'loadPublic') );
 		add_action( 'admin_menu', array($this, 'adminMenu') );
+		add_action( 'wp_loaded', array($this, 'pageLoaded') );
+	}
+
+	public function loadClass($className)
+	{
+		$path = get_template_directory() . '/modules/models/' . substr($className, 7) . '.php';
+		include($path);
+	}
+
+	public function handleException($exc)
+	{
+		print_r($exc);
+	}
+
+	public function pageLoaded()
+	{
+		current_user_can('read');
 	}
 
 	public function adminMenu()
 	{
-	  $menus = require(get_template_directory() . '/modules/controllers/admin/menu.php');
+	  current_user_can('read');
+	  $menus = require(get_template_directory() . '/modules/config/admin_menu.php');
 	  foreach ($menus as $route => $menu) {
-	    add_menu_page($menu[0], $menu[1], 'read', 'c/' . $route, array($this, 'loadAdmin'));
+	    add_menu_page(
+	    	$menu[0], 
+	    	$menu[1], 
+	    	'read', 
+	    	'c/' . $route,
+	    	array($this, 'loadAdmin'));
+	    if (isset($menu[2])) {
+	    	foreach ($menu[2] as $sub_route => $sub_menu) {
+	    		add_submenu_page(
+			    	'c/' . $route,
+			        $sub_menu[0],
+			        $sub_menu[1],
+			        'read',
+			        'c/' . $sub_route,
+			        array($this, 'loadAdmin'));
+	    	}
+	    }
+	    if (isset($menu[3])) {
+	    	foreach ($menu[3] as $sub_route => $sub_menu) {
+	    		add_submenu_page(
+			    	null,
+			        $sub_menu[0],
+			        $sub_menu[1],
+			        'read',
+			        'c/' . $sub_route,
+			        array($this, 'loadAdmin'));
+	    	}
+	    }
 	  }
 	}
 
@@ -31,7 +84,7 @@ class Base
 	{
 		global $plugin_page;
 
-		$routing = require(get_template_directory() . '/modules/admin_routes.php');
+		$routing = require(get_template_directory() . '/modules/config/admin_routes.php');
 		$args = array();
 
 		$view_dir = '/admin';
@@ -45,13 +98,14 @@ class Base
 		$data = require($controller_path);
 
 		$view = new View($view_name, $data);
+		$view->setSeparateAssets(false);
 		$view->parse($view);
 		print $view->getOutput();
 	}
 
 	public function loadPublic()
 	{
-	  $routing = require(get_template_directory() . '/modules/routes.php');
+	  $routing = require(get_template_directory() . '/modules/config/public_routes.php');
 
 	  $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 	  $uri_segments = explode('/', $uri_path);
